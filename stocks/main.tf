ef60e07b6a39d79d2b4e662557df3f92c90b1d82
CM Lubinski <cm.lubinski@gmail.com>
2020-05-17 21:42:12 -0400

    Add cloud scheduler event for testing.

--- stocks/main.tf
+++ stocks/main.tf
@@ -43,6 +43,18 @@ resource "google_cloud_run_service_iam_policy" "noauth" {
   policy_data = data.google_iam_policy.noauth.policy_data
 }
 
+resource "google_cloud_scheduler_job" "job" {
+  name             = "test-job"
+  region           = "us-central1"
+  schedule         = "*/20 * * * *"
+  time_zone        = "America/New_York"
+
+  http_target {
+    http_method = "GET"
+    uri         = google_cloud_run_service.app.status[0].url
+  }
+}
+
 output "service_url" {
   value = google_cloud_run_service.app.status[0].url
 }
