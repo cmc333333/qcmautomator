6afcb31b2cffc1b5b8ac8fb12240493b07b8dbf6
CM Lubinski <cm.lubinski@gmail.com>
2020-06-07 02:35:55 -0400

    Fetch podcast data.
    Like the episode actions, this should backfill data as necessary.

--- gpodder/app.py
+++ gpodder/app.py
@@ -2,6 +2,7 @@ import logging
 from time import time
 
 from mygpoclient.api import MygPodderClient
+from mygpoclient.public import PublicClient
 
 import datastore
 from secrets_config import SecretsConfig
@@ -9,8 +10,7 @@ from secrets_config import SecretsConfig
 logger = logging.getLogger("gpodder")
 
 
-def fetch_episode_actions() -> None:
-    config = SecretsConfig.instance()
+def fetch_episode_actions(config: SecretsConfig) -> None:
     client = MygPodderClient(config.gpodder_username, config.gpodder_password)
     max_action = datastore.max_action() or time() - (60 * 60 * 24 * 45)
     hours = (time() - max_action) / 60 / 60
@@ -25,6 +25,19 @@ def fetch_episode_actions() -> None:
         logger.info("No new actions")
 
 
+def fetch_podcasts(config: SecretsConfig) -> None:
+    client = PublicClient()
+    podcasts = [client.get_podcast_data(url) for url in datastore.missing_podcasts()]
+    if podcasts:
+        datastore.append_podcasts(podcasts)
+        logger.info(f"Added {len(podcasts)} podcasts")
+    else:
+        logger.info("No new podcasts")
+
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    fetch_episode_actions()
+    config = SecretsConfig.instance()
+    datastore.migrate_db()
+    fetch_episode_actions(config)
+    fetch_podcasts(config)
--- gpodder/datastore.py
+++ gpodder/datastore.py
@@ -4,6 +4,8 @@ from typing import Iterable, Optional
 from google.cloud.bigquery import Client
 from google.cloud.bigquery.job import LoadJobConfig, QueryJobConfig
 from mygpoclient.api import EpisodeAction
+from mygpoclient.public import Episode
+from mygpoclient.simple import Podcast
 
 import sqlalchemy
 from sqlalchemy import sql
@@ -16,19 +18,51 @@ _meta = sqlalchemy.MetaData(bind=_engine)
 episode_actions = sqlalchemy.Table(
     "episode_actions",
     _meta,
-    sqlalchemy.Column("podcast", sqlalchemy.String(), nullable=False),
-    sqlalchemy.Column("episode", sqlalchemy.String(), nullable=False),
-    sqlalchemy.Column("action", sqlalchemy.String(), nullable=False),
-    sqlalchemy.Column("device", sqlalchemy.String(), nullable=False),
-    sqlalchemy.Column("timestamp", sqlalchemy.TIMESTAMP(timezone=True), nullable=False),
+    sqlalchemy.Column("podcast", sqlalchemy.String(), nullable=False, primary_key=True),
+    sqlalchemy.Column("episode", sqlalchemy.String(), nullable=False, primary_key=True),
+    sqlalchemy.Column("action", sqlalchemy.String(), nullable=False, primary_key=True),
+    sqlalchemy.Column("device", sqlalchemy.String(), nullable=False, primary_key=True),
+    sqlalchemy.Column(
+        "timestamp",
+        sqlalchemy.TIMESTAMP(timezone=True),
+        nullable=False,
+        primary_key=True,
+    ),
     sqlalchemy.Column("started", sqlalchemy.Integer),
     sqlalchemy.Column("position", sqlalchemy.Integer),
     sqlalchemy.Column("total", sqlalchemy.Integer),
 )
 episode_actions_stage = sqlalchemy.Table(
-    "episode_actions_stage",
+    "episode_actions_stage", _meta, *(col.copy() for col in episode_actions.columns)
+)
+
+podcasts = sqlalchemy.Table(
+    "podcasts",
     _meta,
-    *(col.copy() for col in episode_actions.columns)
+    sqlalchemy.Column("podcast", sqlalchemy.String(), primary_key=True, nullable=False),
+    sqlalchemy.Column("title", sqlalchemy.String(), nullable=False),
+    sqlalchemy.Column("description", sqlalchemy.String()),
+    sqlalchemy.Column("website", sqlalchemy.String()),
+    sqlalchemy.Column("logo", sqlalchemy.String()),
+)
+podcasts_stage = sqlalchemy.Table(
+    "podcasts_stage", _meta, *(col.copy() for col in podcasts.columns)
+)
+
+episodes = sqlalchemy.Table(
+    "episodes",
+    _meta,
+    sqlalchemy.Column("episode", sqlalchemy.String(), nullable=False, primary_key=True),
+    sqlalchemy.Column("podcast", sqlalchemy.String(), nullable=False),
+    sqlalchemy.Column("title", sqlalchemy.String(), nullable=False),
+    sqlalchemy.Column("description", sqlalchemy.String()),
+    sqlalchemy.Column("website", sqlalchemy.String()),
+    sqlalchemy.Column(
+        "released_at", sqlalchemy.TIMESTAMP(timezone=True), nullable=False
+    ),
+)
+episodes_stage = sqlalchemy.Table(
+    "episodes_stage", _meta, *(col.copy() for col in episodes.columns)
 )
 
 
@@ -40,46 +74,104 @@ def migrate_db():
 def max_action() -> Optional[int]:
     with _engine.connect() as conn:
         row = conn.execute(
-            sql.select([sql.func.max(episode_actions.c.timestamp).label("max_ts")])
+            sql.select([sql.func.max(episode_actions.c.timestamp)])
         ).fetchone()
-        return int(row["max_ts"].timestamp()) if row["max_ts"] else None
+        return int(row[0].timestamp()) if row[0] else None
 
 
-def append_actions(actions: Iterable[EpisodeAction]) -> None:
+def missing_podcasts(limit: int = 10) -> Iterable[str]:
+    with _engine.connect() as conn:
+        rows = conn.execute(
+            sql.select(distinct=True, limit=limit, columns=[episode_actions.c.podcast])
+            .select_from(
+                episode_actions.outerjoin(
+                    podcasts, episode_actions.c.podcast == podcasts.c.podcast,
+                )
+            )
+            .where(podcasts.c.podcast.is_(None))
+        ).fetchall()
+        for row in rows:
+            yield row[0]
+
+
+def merge_data(target_sql_tbl: sqlalchemy.Table, data: Iterable[dict]) -> None:
+    """Generic function to merge a chunk of data into a target table, removing any
+    duplicates."""
     client = Client()
-    destination = client.get_table(
-        f"{os.environ['GCP_PROJECT_ID']}"
-        f".{os.environ['APP_NAME']}"
-        ".episode_actions_stage"
+    stage_bq_tbl = client.get_table(
+        f"{_engine.dialect.dataset_id}.{target_sql_tbl.name}_stage"
     )
     load_job = client.load_table_from_json(
-        destination=destination,
-        json_rows=[
-            {col.name: getattr(action, col.name) for col in episode_actions.columns}
-            for action in actions
-        ],
+        destination=stage_bq_tbl,
+        json_rows=data,
         job_config=LoadJobConfig(
-            schema=destination.schema, write_disposition="WRITE_TRUNCATE"
+            schema=stage_bq_tbl.schema, write_disposition="WRITE_TRUNCATE"
         ),
     )
     load_job.result()  # wait for the job to finish
 
+    merge_text = " AND ".join(
+        [
+            f"target.{col.name} = source.{col.name}"
+            for col in target_sql_tbl.columns
+            if col.primary_key
+        ]
+    )
     merge_job = client.query(
-        """
-          MERGE INTO episode_actions target
-          USING episode_actions_stage source
-          ON (
-            target.podcast = source.podcast
-            AND target.episode = source.episode
-            AND target.action = source.action
-            AND target.device = source.device
-            AND target.timestamp = source.timestamp
-          )
+        f"""
+          MERGE INTO {target_sql_tbl.name} target
+          USING {stage_bq_tbl.table_id} source
+          ON ({merge_text})
           WHEN NOT MATCHED BY TARGET THEN
             INSERT ROW
         """,
         QueryJobConfig(
-            default_dataset=f"{destination.project}.{destination.dataset_id}",
+            default_dataset=(
+                f"{os.environ['GCP_PROJECT_ID']}.{_engine.dialect.dataset_id}"
+            ),
         ),
     )
     merge_job.result()
+
+
+def append_actions(actions: Iterable[EpisodeAction]) -> None:
+    merge_data(
+        episode_actions,
+        [
+            {col.name: getattr(action, col.name) for col in episode_actions.columns}
+            for action in actions
+        ],
+    )
+
+
+def append_podcasts(gpo_podcasts: Iterable[Podcast]) -> None:
+    merge_data(
+        podcasts,
+        [
+            {
+                "podcast": podcast.url,
+                "title": podcast.title,
+                "description": podcast.description or None,
+                "website": podcast.website,
+                "logo": podcast.logo_url,
+            }
+            for podcast in gpo_podcasts
+        ],
+    )
+
+
+def append_episodes(gpo_episodes: Iterable[Episode]) -> None:
+    merge_data(
+        episodes,
+        [
+            {
+                "episode": episode.url,
+                "podcast": episode.podcast_url,
+                "title": episode.title,
+                "description": episode.description or None,
+                "website": episode.website,
+                "released_at": episode.released,
+            }
+            for episode in gpo_episodes
+        ],
+    )
