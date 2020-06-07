b5d33cc3ab96ad9a300617c91c91936a1506537d
CM Lubinski <cm.lubinski@gmail.com>
2020-06-06 20:54:25 -0400

    Switch to a multi-stage file load.
    Using "merge" will allow us to remove duplicates relatively easily. This
    also switches to sqlalchemy to avoid defining the table schema in
    multiple places.

--- gpodder/datastore.py
+++ gpodder/datastore.py
@@ -1,61 +1,92 @@
 import os
-from functools import lru_cache
-from typing import Dict, Iterable, Optional
-from urllib.parse import urlparse
+from typing import Iterable, Optional
 
 from google.cloud.bigquery import Client
-from google.cloud.bigquery.dataset import DatasetReference
-from google.cloud.bigquery.job import QueryJobConfig
+from google.cloud.bigquery.job import LoadJobConfig, QueryJobConfig
 from mygpoclient.api import EpisodeAction
-from url_normalize import url_normalize
+
+import sqlalchemy
+from sqlalchemy import sql
+
+_engine = sqlalchemy.create_engine(
+    f"bigquery://{os.environ['GCP_PROJECT_ID']}/{os.environ['APP_NAME']}"
+)
+_meta = sqlalchemy.MetaData(bind=_engine)
+
+episode_actions = sqlalchemy.Table(
+    "episode_actions",
+    _meta,
+    sqlalchemy.Column("podcast", sqlalchemy.String(), nullable=False),
+    sqlalchemy.Column("episode", sqlalchemy.String(), nullable=False),
+    sqlalchemy.Column("action", sqlalchemy.String(), nullable=False),
+    sqlalchemy.Column("device", sqlalchemy.String(), nullable=False),
+    sqlalchemy.Column("timestamp", sqlalchemy.TIMESTAMP(timezone=True), nullable=False),
+    sqlalchemy.Column("started", sqlalchemy.Integer),
+    sqlalchemy.Column("position", sqlalchemy.Integer),
+    sqlalchemy.Column("total", sqlalchemy.Integer),
+)
+episode_actions_stage = sqlalchemy.Table(
+    "episode_actions_stage",
+    _meta,
+    sqlalchemy.Column("podcast", sqlalchemy.String(), nullable=False),
+    sqlalchemy.Column("episode", sqlalchemy.String(), nullable=False),
+    sqlalchemy.Column("action", sqlalchemy.String(), nullable=False),
+    sqlalchemy.Column("device", sqlalchemy.String(), nullable=False),
+    sqlalchemy.Column("timestamp", sqlalchemy.TIMESTAMP(timezone=True), nullable=False),
+    sqlalchemy.Column("started", sqlalchemy.Integer),
+    sqlalchemy.Column("position", sqlalchemy.Integer),
+    sqlalchemy.Column("total", sqlalchemy.Integer),
+)
 
 
-@lru_cache
-def bq_client() -> Client:
-    return Client()
+def migrate_db():
+    """Update the table schema to the latest. Naive solution for now."""
+    _meta.create_all()
 
 
 def max_action() -> Optional[int]:
-    config = QueryJobConfig(
-        default_dataset=DatasetReference(
-            os.environ["GCP_PROJECT_ID"], os.environ["APP_NAME"]
-        ),
-    )
-    job = bq_client().query("SELECT MAX(timestamp) FROM episode_actions", config)
-    for row in job:
-        if row[0]:
-            return int(row[0].timestamp())
-    return None
-
-
-def url_to_struct(raw: str) -> Dict[str, str]:
-    normalized = url_normalize(raw)
-    parsed = urlparse(normalized)
-    return {
-        "raw": raw,
-        "normalized": normalized,
-        "domain": parsed.netloc,
-        "base_name": f"{parsed.scheme}://{parsed.netloc}{parsed.path}",
-    }
+    with _engine.connect() as conn:
+        row = conn.execute(
+            sql.select([sql.func.max(episode_actions.c.timestamp).label("max_ts")])
+        ).fetchone()
+        return int(row["max_ts"].timestamp()) if row else None
 
 
 def append_actions(actions: Iterable[EpisodeAction]) -> None:
-    data = [
-        {
-            "podcast": url_to_struct(action.podcast),
-            "episode": url_to_struct(action.episode),
-            "action": action.action,
-            "device": action.device,
-            "timestamp": action.timestamp,
-            "started": action.started,
-            "position": action.position,
-            "total": action.total,
-        }
-        for action in actions
-    ]
-    table = ".".join(
-        [os.environ["GCP_PROJECT_ID"], os.environ["APP_NAME"], "episode_actions"]
+    client = Client()
+    destination = client.get_table(
+        f"{os.environ['GCP_PROJECT_ID']}"
+        f".{os.environ['APP_NAME']}"
+        ".episode_actions_stage"
     )
-    errors = bq_client().insert_rows_json(table, data)
-    if errors:
-        raise ValueError(errors)
+    load_job = client.load_table_from_json(
+        destination=destination,
+        json_rows=[
+            {col.name: getattr(action, col.name) for col in episode_actions.columns}
+            for action in actions
+        ],
+        job_config=LoadJobConfig(
+            schema=destination.schema, write_disposition="WRITE_TRUNCATE"
+        ),
+    )
+    load_job.result()  # wait for the job to finish
+
+    merge_job = client.query(
+        """
+          MERGE INTO episode_actions target
+          USING episode_actions_stage source
+          ON (
+            target.podcast = source.podcast
+            AND target.episode = source.episode
+            AND target.action = source.action
+            AND target.device = source.device
+            AND target.timestamp = source.timestamp
+          )
+          WHEN NOT MATCHED BY TARGET THEN
+            INSERT ROW
+        """,
+        QueryJobConfig(
+            default_dataset=f"{destination.project}.{destination.dataset_id}",
+        ),
+    )
+    merge_job.result()
