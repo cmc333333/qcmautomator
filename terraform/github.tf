28850eed8d03b6e942f55b045b3c309ab55a3be8
CM Lubinski <cm.lubinski@gmail.com>
2024-03-25 02:32:50 +0000

    Track high water mark for GitHub events.
    This'll prevent us from creating duplicate records.

--- terraform/github.tf
+++ terraform/github.tf
@@ -28,3 +28,43 @@ resource "google_workflows_workflow" "github_events" {
   service_account = google_service_account.github.email
   source_contents = file("../workflows/github/events.workflows.yml")
 }
+
+resource "google_project_iam_member" "github_bq_user" {
+  project = google_project_service.bigquery.project
+  role    = "roles/bigquery.jobUser"
+  member  = google_service_account.github.member
+}
+
+resource "google_bigquery_dataset" "github" {
+  dataset_id = "github"
+}
+
+resource "google_bigquery_dataset_access" "github_reading" {
+  dataset_id    = google_bigquery_dataset.github.dataset_id
+  role          = "READER"
+  user_by_email = google_service_account.github.email
+}
+
+resource "google_bigquery_table" "github_latest_id" {
+  dataset_id          = google_bigquery_dataset.github.dataset_id
+  table_id            = "latest_id"
+  deletion_protection = false
+  view {
+    use_legacy_sql = false
+    query          = <<-EOT
+      SELECT MAX(jsonPayload.github_event_v1.id) AS id
+      FROM `${google_bigquery_dataset.raw_data.dataset_id}.Workflows`
+    EOT
+  }
+}
+
+resource "google_bigquery_dataset_access" "raw_data_github" {
+  dataset_id = google_bigquery_dataset.raw_data.dataset_id
+  dataset {
+    target_types = ["VIEWS"]
+    dataset {
+      project_id = google_bigquery_dataset.github.project
+      dataset_id = google_bigquery_dataset.github.dataset_id
+    }
+  }
+}
